b28154a1d111afc4341b502c221f57cf320ff3b6
Jaehyun Ahn <desktop3711@gmail.com>
2025-07-30 20:05:07 +0900

    feat: 참여자 정보 VO 추가

---
+++ src/main/java/com/gdschongik/gdsc/domain/event/domain/Participant.java
@@ -0,0 +1,20 @@
+package com.gdschongik.gdsc.domain.event.domain;
+
+import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
+
+import com.gdschongik.gdsc.domain.member.domain.Member;
+import com.gdschongik.gdsc.global.exception.CustomException;
+
+public record Participant(String name, String studentId, String phone) {
+
+    public static Participant of(String name, String studentId, String phone) {
+        return new Participant(name, studentId, phone);
+    }
+
+    public static Participant from(Member member) {
+        if (!member.getAssociateRequirement().isInfoSatisfied()) {
+            throw new CustomException(PARTICIPANT_NOT_CREATABLE_INFO_NOT_SATISFIED);
+        }
+        return new Participant(member.getName(), member.getStudentId(), member.getPhone());
+    }
+}
