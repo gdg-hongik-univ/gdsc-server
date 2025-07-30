69d07c5adbe5974a9ab85587cf49d89c8ad935db
Jaehyun Ahn <desktop3711@gmail.com>
2025-07-30 20:29:31 +0900

    feat: 이벤트 신청상태 및 참여상태 추가

---
+++ src/main/java/com/gdschongik/gdsc/domain/event/domain/EventApplicationStatus.java
@@ -0,0 +1,8 @@
+package com.gdschongik.gdsc.domain.event.domain;
+
+public enum EventApplicationStatus {
+    NONE,
+    NOT_APPLIED,
+    APPLIED,
+    ;
+}
---
+++ src/main/java/com/gdschongik/gdsc/domain/event/domain/EventAttendanceStatus.java
@@ -0,0 +1,8 @@
+package com.gdschongik.gdsc.domain.event.domain;
+
+public enum EventAttendanceStatus {
+    NONE,
+    NOT_ATTENDED,
+    ATTENDED,
+    ;
+}
