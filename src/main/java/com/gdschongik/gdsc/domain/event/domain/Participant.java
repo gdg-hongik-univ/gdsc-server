d70899ff80dbedbab4f676e79092db0974b46eaa
Jaehyun Ahn <desktop3711@gmail.com>
2025-07-31 20:26:36 +0900

    feat: 학번 유니크 제약조건 추가

--- src/main/java/com/gdschongik/gdsc/domain/event/domain/Participant.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/domain/Participant.java
@@ -4,10 +4,11 @@ import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.global.exception.CustomException;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
-public record Participant(String name, String studentId, String phone) {
+public record Participant(String name, @Column(unique = true) String studentId, String phone) {
 
     public static Participant of(String name, String studentId, String phone) {
         return new Participant(name, studentId, phone);
