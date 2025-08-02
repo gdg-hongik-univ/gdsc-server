72215e98aafaf98f662d3ba5ffef1f80cc25db40
Jaehyun Ahn <desktop3711@gmail.com>
2025-08-02 21:51:38 +0900

    feat: record 대신 일반 클래스로 전환

--- src/main/java/com/gdschongik/gdsc/domain/event/domain/Participant.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/domain/Participant.java
@@ -6,12 +6,38 @@ import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.global.exception.CustomException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Embeddable
-public record Participant(String name, @Column(unique = true) String studentId, String phone) {
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public final class Participant {
+
+    private String name;
+
+    @Column(unique = true)
+    private String studentId;
+
+    private String phone;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Participant(String name, String studentId, String phone) {
+        this.name = name;
+        this.studentId = studentId;
+        this.phone = phone;
+    }
 
     public static Participant of(String name, String studentId, String phone) {
-        return new Participant(name, studentId, phone);
+        return Participant.builder()
+                .name(name)
+                .studentId(studentId)
+                .phone(phone)
+                .build();
     }
 
     public static Participant from(Member member) {
@@ -19,6 +45,10 @@ public record Participant(String name, @Column(unique = true) String studentId, 
             throw new CustomException(PARTICIPANT_NOT_CREATABLE_INFO_NOT_SATISFIED);
         }
 
-        return new Participant(member.getName(), member.getStudentId(), member.getPhone());
+        return Participant.builder()
+                .name(member.getName())
+                .studentId(member.getStudentId())
+                .phone(member.getPhone())
+                .build();
     }
 }
