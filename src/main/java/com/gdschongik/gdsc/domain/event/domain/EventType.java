68b7e4053a1312c6ce2256e81f9f91140ed11f43
kimsh1017 <kimsh8580@gmail.com>
2025-08-02 12:02:38 +0900

    feat: enum 생성자 변경

--- src/main/java/com/gdschongik/gdsc/domain/event/domain/EventType.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/domain/EventType.java
@@ -2,9 +2,10 @@ package com.gdschongik.gdsc.domain.event.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public enum EventType {
     ONLINE("온라인 행사"),
     OFFLINE("오프라인 행사"),
--- src/main/java/com/gdschongik/gdsc/domain/event/domain/UsageStatus.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/domain/UsageStatus.java
@@ -2,9 +2,10 @@ package com.gdschongik.gdsc.domain.event.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public enum UsageStatus {
     ENABLED("활성화"),
     DISABLED("비활성화");
