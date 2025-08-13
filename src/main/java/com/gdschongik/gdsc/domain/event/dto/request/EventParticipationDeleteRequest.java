0678f472f6eaacc09a0fa3dc37337f53ecee7490
kimsh1017 <kimsh8580@gmail.com>
2025-08-13 22:57:50 +0900

    refactor: id null 여부 검증 추가

--- src/main/java/com/gdschongik/gdsc/domain/event/dto/request/EventParticipationDeleteRequest.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/dto/request/EventParticipationDeleteRequest.java
@@ -1,7 +1,8 @@
 package com.gdschongik.gdsc.domain.event.dto.request;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.util.List;
 
-public record EventParticipationDeleteRequest(@NotEmpty List<@Positive Long> eventParticipationIds) {}
+public record EventParticipationDeleteRequest(@NotEmpty List<@NotNull @Positive Long> eventParticipationIds) {}
