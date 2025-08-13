983313f888f3476dc2541353259133f929c3689d
kimsh1017 <kimsh8580@gmail.com>
2025-08-13 22:55:52 +0900

    refactor: id 양수 여부 검증 추가

--- src/main/java/com/gdschongik/gdsc/domain/event/dto/request/EventParticipationDeleteRequest.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/dto/request/EventParticipationDeleteRequest.java
@@ -1,6 +1,7 @@
 package com.gdschongik.gdsc.domain.event.dto.request;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 
-public record EventParticipationDeleteRequest(@NotEmpty List<Long> eventParticipationIds) {}
+public record EventParticipationDeleteRequest(@NotEmpty List<@Positive Long> eventParticipationIds) {}
