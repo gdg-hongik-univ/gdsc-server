73a868026a7d4c873b520ecc5452e5b276ef92b8
kimsh1017 <kimsh8580@gmail.com>
2025-08-14 22:26:11 +0900

    feat: 뒤풀이 정보 조회 API 추가

--- src/main/java/com/gdschongik/gdsc/domain/event/api/AdminEventParticipationController.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/api/AdminEventParticipationController.java
@@ -1,10 +1,21 @@
 package com.gdschongik.gdsc.domain.event.api;
 
+import com.gdschongik.gdsc.domain.event.domain.AfterPartyApplicationStatus;
+import com.gdschongik.gdsc.domain.event.domain.AfterPartyAttendanceStatus;
+import com.gdschongik.gdsc.domain.event.domain.MainEventApplicationStatus;
+import com.gdschongik.gdsc.domain.event.domain.PaymentStatus;
+import com.gdschongik.gdsc.domain.event.dto.request.EventParticipantQueryOption;
 import com.gdschongik.gdsc.domain.event.dto.request.EventParticipationDeleteRequest;
+import com.gdschongik.gdsc.domain.event.dto.response.AfterPartyParticipationResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,5 +30,29 @@ public class AdminEventParticipationController {
     public ResponseEntity<Void> deleteEventParticipations(@Valid @RequestBody EventParticipationDeleteRequest request) {
         // TODO: 서비스 로직 구현
         return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "뒤풀이 신청 정보 조회", description = "뒤풀이 신청 정보를 조회합니다.")
+    @GetMapping("/after-party")
+    public ResponseEntity<Page<AfterPartyParticipationResponse>> getAfterPartyParticipations(
+            @RequestParam(name = "eventId") Long eventId,
+            @ParameterObject EventParticipantQueryOption queryOption,
+            @ParameterObject Pageable pageable) {
+
+        // TODO: 임시 응답 제거 후 서비스 로직 구현
+        var exampleContent = List.of(new AfterPartyParticipationResponse(
+                1L,
+                "김홍익",
+                "C123456",
+                "010-1234-5678",
+                1L,
+                MainEventApplicationStatus.APPLIED,
+                AfterPartyApplicationStatus.APPLIED,
+                AfterPartyAttendanceStatus.NOT_ATTENDED,
+                PaymentStatus.UNPAID,
+                PaymentStatus.UNPAID));
+
+        var exampleResponse = new PageImpl<>(exampleContent, pageable, 1L);
+        return ResponseEntity.ok(exampleResponse);
     }
 }
