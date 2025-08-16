ee6f4d464236219ae9c0cba99685f0b7cca242fa
Jaehyun Ahn <desktop3711@gmail.com>
2025-08-17 00:31:09 +0900

    feat: 행사 신청자 목록 조회 임시 API 구현

--- src/main/java/com/gdschongik/gdsc/domain/event/api/AdminEventParticipationController.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/api/AdminEventParticipationController.java
@@ -3,11 +3,14 @@ package com.gdschongik.gdsc.domain.event.api;
 import com.gdschongik.gdsc.domain.event.domain.AfterPartyApplicationStatus;
 import com.gdschongik.gdsc.domain.event.domain.AfterPartyAttendanceStatus;
 import com.gdschongik.gdsc.domain.event.domain.MainEventApplicationStatus;
+import com.gdschongik.gdsc.domain.event.domain.Participant;
+import com.gdschongik.gdsc.domain.event.domain.ParticipantRole;
 import com.gdschongik.gdsc.domain.event.domain.PaymentStatus;
 import com.gdschongik.gdsc.domain.event.dto.EventParticipationDto;
 import com.gdschongik.gdsc.domain.event.dto.ParticipantDto;
 import com.gdschongik.gdsc.domain.event.dto.request.EventParticipantQueryOption;
 import com.gdschongik.gdsc.domain.event.dto.request.EventParticipationDeleteRequest;
+import com.gdschongik.gdsc.domain.event.dto.response.EventApplicantResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -50,6 +53,23 @@ public class AdminEventParticipationController {
                 AfterPartyAttendanceStatus.NOT_ATTENDED,
                 PaymentStatus.UNPAID,
                 PaymentStatus.UNPAID));
+
+        var exampleResponse = new PageImpl<>(exampleContent, pageable, 1L);
+        return ResponseEntity.ok(exampleResponse);
+    }
+
+    @Operation(summary = "행사 신청자 목록 조회", description = "해당 행사의 신청자 목록을 조회합니다")
+    @GetMapping("/applicants")
+    public ResponseEntity<Page<EventApplicantResponse>> getEventApplicants(
+            @RequestParam(name = "event") Long eventId,
+            @ParameterObject EventParticipantQueryOption queryOption,
+            @ParameterObject Pageable pageable) {
+
+        // TODO: 임시 응답 제거 후 서비스 로직 구현
+        var exampleContent = List.of(new EventApplicantResponse(
+                Participant.of("김홍익", "C123456", "01012345678"),
+                AfterPartyApplicationStatus.APPLIED,
+                ParticipantRole.NON_MEMBER));
 
         var exampleResponse = new PageImpl<>(exampleContent, pageable, 1L);
         return ResponseEntity.ok(exampleResponse);
