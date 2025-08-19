cca85c1fa0a7941e842430a77efc87b691741b63
kimsh1017 <kimsh8580@gmail.com>
2025-08-19 22:22:15 +0900

    refactor: DTO 대신 VO 사용하도록 변경

--- src/main/java/com/gdschongik/gdsc/domain/event/dto/EventParticipationDto.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/dto/EventParticipationDto.java
@@ -1,13 +1,10 @@
 package com.gdschongik.gdsc.domain.event.dto;
 
-import com.gdschongik.gdsc.domain.event.domain.AfterPartyApplicationStatus;
-import com.gdschongik.gdsc.domain.event.domain.AfterPartyAttendanceStatus;
-import com.gdschongik.gdsc.domain.event.domain.MainEventApplicationStatus;
-import com.gdschongik.gdsc.domain.event.domain.PaymentStatus;
+import com.gdschongik.gdsc.domain.event.domain.*;
 
 public record EventParticipationDto(
         Long eventParticipationId,
-        ParticipantDto participant,
+        Participant participant,
         Long memberId,
         MainEventApplicationStatus mainEventApplicationStatus,
         AfterPartyApplicationStatus afterPartyApplicationStatus,
