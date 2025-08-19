8d436d5a0408892b959ab7cae4c2c3b8bc93492b
kimsh1017 <kimsh8580@gmail.com>
2025-08-19 22:38:30 +0900

    refactor: 임시 응답 변경

--- src/main/java/com/gdschongik/gdsc/domain/event/api/AdminEventParticipationController.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/api/AdminEventParticipationController.java
@@ -1,12 +1,8 @@
 package com.gdschongik.gdsc.domain.event.api;
 
 import com.gdschongik.gdsc.domain.event.application.EventParticipationService;
-import com.gdschongik.gdsc.domain.event.domain.AfterPartyApplicationStatus;
-import com.gdschongik.gdsc.domain.event.domain.AfterPartyAttendanceStatus;
-import com.gdschongik.gdsc.domain.event.domain.MainEventApplicationStatus;
-import com.gdschongik.gdsc.domain.event.domain.PaymentStatus;
-import com.gdschongik.gdsc.domain.event.dto.EventParticipationDto;
-import com.gdschongik.gdsc.domain.event.dto.ParticipantDto;
+import com.gdschongik.gdsc.domain.event.domain.*;
+import com.gdschongik.gdsc.domain.event.dto.dto.EventParticipationDto;
 import com.gdschongik.gdsc.domain.event.dto.request.EventParticipantQueryOption;
 import com.gdschongik.gdsc.domain.event.dto.request.EventParticipationDeleteRequest;
 import com.gdschongik.gdsc.domain.event.dto.response.AfterPartyAttendanceResponse;
@@ -18,7 +14,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -48,7 +43,7 @@ public class AdminEventParticipationController {
         // TODO: 임시 응답 제거 후 서비스 로직 구현
         var exampleContent = List.of(new EventParticipationDto(
                 1L,
-                new ParticipantDto("김홍익", "C123456", "01012345678"),
+                Participant.of("김홍익", "C123456", "01012345678"),
                 1L,
                 MainEventApplicationStatus.APPLIED,
                 AfterPartyApplicationStatus.APPLIED,
@@ -56,7 +51,11 @@ public class AdminEventParticipationController {
                 PaymentStatus.UNPAID,
                 PaymentStatus.UNPAID));
 
-        var exampleResponse = new PageImpl<>(exampleContent, pageable, 1L);
+        var exampleResponse = AfterPartyAttendanceResponse.of(
+                5L, // attendedAfterApplyingCount
+                2L, // notAttendedAfterApplyingCount
+                3L, // onSiteApplicationCount
+                exampleContent);
         return ResponseEntity.ok(exampleResponse);
     }
 
