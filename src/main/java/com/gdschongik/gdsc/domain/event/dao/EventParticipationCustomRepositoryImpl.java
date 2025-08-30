9a72bebe56a4799c301df5b776882c963362520e
kimsh1017 <kimsh8580@gmail.com>
2025-08-30 13:24:51 +0900

    refactor: dao 로직 구현

--- src/main/java/com/gdschongik/gdsc/domain/event/dao/EventParticipationCustomRepositoryImpl.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/dao/EventParticipationCustomRepositoryImpl.java
@@ -1,15 +1,23 @@
 package com.gdschongik.gdsc.domain.event.dao;
 
+import static com.gdschongik.gdsc.domain.event.domain.AfterPartyApplicationStatus.APPLIED;
+import static com.gdschongik.gdsc.domain.event.domain.AfterPartyAttendanceStatus.ATTENDED;
+import static com.gdschongik.gdsc.domain.event.domain.PaymentStatus.*;
 import static com.gdschongik.gdsc.domain.event.domain.QEventParticipation.*;
 import static com.gdschongik.gdsc.domain.member.domain.QMember.*;
 import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
+import com.gdschongik.gdsc.domain.event.dto.dto.AfterPartyApplicantCountDto;
+import com.gdschongik.gdsc.domain.event.dto.dto.EventParticipationDto;
+import com.gdschongik.gdsc.domain.event.dto.dto.QAfterPartyApplicantCountDto;
 import com.gdschongik.gdsc.domain.event.dto.request.EventParticipantQueryOption;
+import com.gdschongik.gdsc.domain.event.dto.response.AfterPartyApplicantResponse;
 import com.gdschongik.gdsc.domain.event.dto.response.EventApplicantResponse;
 import com.gdschongik.gdsc.domain.event.dto.response.QEventApplicantResponse;
 import com.gdschongik.gdsc.global.exception.CustomException;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.ArrayList;
 import java.util.List;
@@ -78,8 +86,70 @@ public class EventParticipationCustomRepositoryImpl
                 .from(eventParticipation)
                 .leftJoin(member)
                 .on(eqMemberId())
-                .where(eqEventId(eventId), matchesEventParticipantQueryOption(queryOption), null)
+                .where(eqEventId(eventId), matchesEventParticipantQueryOption(queryOption))
                 .orderBy(orderSpecifiers)
                 .fetch();
+    }
+
+    @Override
+    public AfterPartyApplicantResponse findAfterPartyApplicants(
+            Long eventId, EventParticipantQueryOption queryOption, Pageable pageable) {
+
+        OrderSpecifier<?>[] orderSpecifiers = getOrderSpecifiers(pageable);
+        List<Long> ids = getAfterPartyApplicantIdsByQueryOption(eventId, queryOption, orderSpecifiers);
+
+        List<EventParticipationDto> fetch = jpaQueryFactory
+                .selectFrom(eventParticipation)
+                .where(eventParticipation.id.in(ids))
+                .orderBy(orderSpecifiers)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .stream()
+                .map(EventParticipationDto::from)
+                .toList();
+
+        AfterPartyApplicantCountDto counts = jpaQueryFactory
+                .select(getEventParticipationCounts())
+                .from(eventParticipation)
+                .fetchOne();
+
+        return AfterPartyApplicantResponse.of(PageableExecutionUtils.getPage(fetch, pageable, ids::size), counts);
+    }
+
+    private List<Long> getAfterPartyApplicantIdsByQueryOption(
+            Long eventId, EventParticipantQueryOption queryOption, @NonNull OrderSpecifier<?>... orderSpecifiers) {
+        return jpaQueryFactory
+                .select(eventParticipation.id)
+                .from(eventParticipation)
+                .where(
+                        eqEventId(eventId),
+                        eqAfterPartyApplicationStatus(APPLIED).or(eqAfterPartyAttendanceStatus(ATTENDED)),
+                        matchesEventParticipantQueryOption(queryOption))
+                .orderBy(orderSpecifiers)
+                .fetch();
+    }
+
+    private QAfterPartyApplicantCountDto getEventParticipationCounts() {
+        return new QAfterPartyApplicantCountDto(
+                Expressions.cases()
+                        .when(eqAfterPartyApplicationStatus(APPLIED).or(eqAfterPartyAttendanceStatus(ATTENDED)))
+                        .then(eventParticipation.id)
+                        .otherwise((Long) null)
+                        .count(),
+                Expressions.cases()
+                        .when(eqPrePaymentStatus(PAID))
+                        .then(eventParticipation.id)
+                        .otherwise((Long) null)
+                        .count(),
+                Expressions.cases()
+                        .when(eqAfterPartyAttendanceStatus(ATTENDED))
+                        .then(eventParticipation.id)
+                        .otherwise((Long) null)
+                        .count(),
+                Expressions.cases()
+                        .when(eqPostPaymentStatus(PAID))
+                        .then(eventParticipation.id)
+                        .otherwise((Long) null)
+                        .count());
     }
 }
--- src/main/java/com/gdschongik/gdsc/domain/event/dao/EventParticipationQueryMethod.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/dao/EventParticipationQueryMethod.java
@@ -3,6 +3,9 @@ package com.gdschongik.gdsc.domain.event.dao;
 import static com.gdschongik.gdsc.domain.event.domain.QEventParticipation.*;
 import static com.gdschongik.gdsc.domain.member.domain.QMember.*;
 
+import com.gdschongik.gdsc.domain.event.domain.AfterPartyApplicationStatus;
+import com.gdschongik.gdsc.domain.event.domain.AfterPartyAttendanceStatus;
+import com.gdschongik.gdsc.domain.event.domain.PaymentStatus;
 import com.gdschongik.gdsc.domain.event.dto.request.EventParticipantQueryOption;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
@@ -34,5 +37,21 @@ public interface EventParticipationQueryMethod {
 
     default BooleanExpression eqPhone(String phone) {
         return phone != null ? eventParticipation.participant.phone.contains(phone.replaceAll("-", "")) : null;
+    }
+
+    default BooleanExpression eqAfterPartyApplicationStatus(AfterPartyApplicationStatus status) {
+        return status != null ? eventParticipation.afterPartyApplicationStatus.eq(status) : null;
+    }
+
+    default BooleanExpression eqAfterPartyAttendanceStatus(AfterPartyAttendanceStatus status) {
+        return status != null ? eventParticipation.afterPartyAttendanceStatus.eq(status) : null;
+    }
+
+    default BooleanExpression eqPrePaymentStatus(PaymentStatus status) {
+        return status != null ? eventParticipation.prePaymentStatus.eq(status) : null;
+    }
+
+    default BooleanExpression eqPostPaymentStatus(PaymentStatus status) {
+        return status != null ? eventParticipation.postPaymentStatus.eq(status) : null;
     }
 }
