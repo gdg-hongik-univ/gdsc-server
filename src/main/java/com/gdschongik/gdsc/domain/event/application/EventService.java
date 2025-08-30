05081ce19833f1982b0590864ef78554ab80d123
chosw1002@naver.com <chosw1002@naver.com>
2025-08-30 22:39:17 +0900

    refactor: toList로 수정

--- src/main/java/com/gdschongik/gdsc/domain/event/application/EventService.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/application/EventService.java
@@ -4,7 +4,6 @@ import com.gdschongik.gdsc.domain.event.dao.EventParticipationRepository;
 import com.gdschongik.gdsc.domain.event.dao.EventRepository;
 import com.gdschongik.gdsc.domain.event.domain.Event;
 import com.gdschongik.gdsc.domain.event.dto.response.EventResponse;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -24,11 +23,9 @@ public class EventService {
     public Page<EventResponse> getEvents(Pageable pageable) {
         Page<Event> events = eventRepository.findAll(pageable);
 
-        List<EventResponse> response = new ArrayList<>();
-        events.forEach(event -> {
-            long countByEvent = eventParticipationRepository.countByEvent(event);
-            response.add(EventResponse.of(event, countByEvent));
-        });
+        List<EventResponse> response = events.stream()
+                .map(event -> EventResponse.of(event, eventParticipationRepository.countByEvent(event)))
+                .toList();
 
         return new PageImpl<>(response, pageable, events.getTotalElements());
     }
