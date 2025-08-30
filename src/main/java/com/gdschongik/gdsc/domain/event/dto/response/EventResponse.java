0a75e617de72e2be7d08b60cd2a093518e6773f5
chosw1002@naver.com <chosw1002@naver.com>
2025-08-30 22:49:53 +0900

    rename: enum 네이밍 수정

--- src/main/java/com/gdschongik/gdsc/domain/event/dto/response/EventResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/dto/response/EventResponse.java
@@ -7,18 +7,17 @@ import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-public record EventResponse(EventDto event, Long totalAttendeesCount, EventStatusType eventStatusType) {
+public record EventResponse(EventDto event, Long totalAttendeesCount, EventStatus eventStatus) {
 
     public static EventResponse of(Event event, Long totalAttendeesCount) {
         EventDto eventDto = EventDto.from(event);
-        EventStatusType eventStatusType =
-                EventStatusType.of(event.getApplicationPeriod(), event.getStartAt(), LocalDateTime.now());
-        return new EventResponse(eventDto, totalAttendeesCount, eventStatusType);
+        EventStatus eventStatus = EventStatus.of(event.getApplicationPeriod(), event.getStartAt(), LocalDateTime.now());
+        return new EventResponse(eventDto, totalAttendeesCount, eventStatus);
     }
 
     @Getter
     @RequiredArgsConstructor
-    public enum EventStatusType {
+    public enum EventStatus {
         BEFORE_APPLICATION("신청 전"),
         APPLICATION_OPEN("신청 중"),
         APPLICATION_CLOSED("신청 종료"),
@@ -26,7 +25,7 @@ public record EventResponse(EventDto event, Long totalAttendeesCount, EventStatu
 
         private final String value;
 
-        private static EventStatusType of(Period applicationPeriod, LocalDateTime startAt, LocalDateTime now) {
+        private static EventStatus of(Period applicationPeriod, LocalDateTime startAt, LocalDateTime now) {
             if (now.isBefore(applicationPeriod.getStartDate())) {
                 return BEFORE_APPLICATION;
             } else if (applicationPeriod.isWithin(now)) {
