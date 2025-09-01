83cdbba44e140fc23c64c1391b0a800dbc874f2e
kimsh1017 <kimsh8580@gmail.com>
2025-09-01 20:59:09 +0900

    refactor: 입력 dto 제약 어노테이션 변경

--- src/main/java/com/gdschongik/gdsc/domain/event/dto/request/EventCreateRequest.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/dto/request/EventCreateRequest.java
@@ -2,15 +2,16 @@ package com.gdschongik.gdsc.domain.event.dto.request;
 
 import com.gdschongik.gdsc.domain.common.vo.Period;
 import com.gdschongik.gdsc.domain.event.domain.UsageStatus;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.time.LocalDateTime;
 
 public record EventCreateRequest(
-        @NotNull String name,
+        @NotBlank String name,
         String venue,
         @NotNull LocalDateTime startAt,
-        String applicationDescription,
+        @NotBlank String applicationDescription,
         @NotNull Period applicationPeriod,
         @NotNull UsageStatus regularRoleOnlyStatus,
         @NotNull UsageStatus afterPartyStatus,
