e0659d8741b15ca0904ebb513605ffe6a0000f3d
chosw1002@naver.com <chosw1002@naver.com>
2025-09-02 21:49:21 +0900

    feat: 이벤트 검색 api 구현

--- src/main/java/com/gdschongik/gdsc/domain/event/api/AdminEventController.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/api/AdminEventController.java
@@ -1,11 +1,13 @@
 package com.gdschongik.gdsc.domain.event.api;
 
 import com.gdschongik.gdsc.domain.event.application.EventService;
+import com.gdschongik.gdsc.domain.event.dto.dto.EventDto;
 import com.gdschongik.gdsc.domain.event.dto.request.EventCreateRequest;
 import com.gdschongik.gdsc.domain.event.dto.response.EventResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
@@ -33,5 +35,13 @@ public class AdminEventController {
     public ResponseEntity<Void> createEvent(@Valid @RequestBody EventCreateRequest request) {
         eventService.createEvent(request);
         return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "이벤트 검색", description = "이벤트를 검색합니다.")
+    @GetMapping("/search")
+    public ResponseEntity<List<EventDto>> searchEvent(
+            @RequestParam(name = "event", required = false) String eventName) {
+        var response = eventService.searchEvent(eventName);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/gdschongik/gdsc/domain/event/dao/EventRepository.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/dao/EventRepository.java
@@ -1,6 +1,10 @@
 package com.gdschongik.gdsc.domain.event.dao;
 
 import com.gdschongik.gdsc.domain.event.domain.Event;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventRepository extends JpaRepository<Event, Long> {}
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    List<Event> findAllByNameContains(String eventName);
+}
