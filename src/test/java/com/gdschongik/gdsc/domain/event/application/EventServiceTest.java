cb779de3200ac6e5a25dd582a47a640bb1b5ea86
kimsh1017 <kimsh8580@gmail.com>
2025-09-04 20:43:24 +0900

    test: 없는 이벤트 수정 테스트 케이스 추가

--- src/test/java/com/gdschongik/gdsc/domain/event/application/EventServiceTest.java
+++ src/test/java/com/gdschongik/gdsc/domain/event/application/EventServiceTest.java
@@ -4,8 +4,10 @@ import static com.gdschongik.gdsc.global.common.constant.EventConstant.*;
 import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 import static org.assertj.core.api.Assertions.*;
 
+import com.gdschongik.gdsc.domain.event.domain.Event;
 import com.gdschongik.gdsc.domain.event.dto.request.EventCreateRequest;
 import com.gdschongik.gdsc.domain.event.dto.request.EventUpdateRequest;
+import com.gdschongik.gdsc.global.exception.CustomException;
 import com.gdschongik.gdsc.helper.IntegrationTest;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -45,9 +47,29 @@ public class EventServiceTest extends IntegrationTest {
     class 이벤트_수정시 {
 
         @Test
+        void 존재하지_않는_이벤트일_경우_실패한다() {
+            // given
+            String updatedName = "수정된 행사 이름";
+            var request = new EventUpdateRequest(
+                    updatedName,
+                    VENUE,
+                    EVENT_START_AT,
+                    APPLICATION_DESCRIPTION,
+                    EVENT_APPLICATION_PERIOD,
+                    MAIN_EVENT_MAX_APPLICATION_COUNT,
+                    AFTER_PARTY_MAX_APPLICATION_COUNT);
+
+            // when & then
+            assertThatThrownBy(() -> eventService.updateEvent(1L, request))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessageContaining(EVENT_NOT_FOUND.getMessage());
+        }
+
+        @Test
         void 성공한다() {
             // given
-            createEvent();
+            Event event = createEvent();
+            Long eventId = event.getId();
 
             String updatedName = "수정된 행사 이름";
             var request = new EventUpdateRequest(
@@ -60,10 +82,10 @@ public class EventServiceTest extends IntegrationTest {
                     AFTER_PARTY_MAX_APPLICATION_COUNT);
 
             // when
-            eventService.updateEvent(1L, request);
+            eventService.updateEvent(eventId, request);
 
             // then
-            assertThat(eventRepository.findById(1L).get().getName()).isEqualTo(updatedName);
+            assertThat(eventRepository.findById(eventId).get().getName()).isEqualTo(updatedName);
         }
     }
 }
