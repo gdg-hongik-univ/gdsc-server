9e2a5209abcb4195f10f9572ddd1a7724291809a
kckc0608 <kckc0608@naver.com>
2025-09-08 01:26:41 +0900

    docs: 참석을 신청으로 변경

--- src/main/java/com/gdschongik/gdsc/domain/event/api/AdminEventParticipationController.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/api/AdminEventParticipationController.java
@@ -87,7 +87,7 @@ public class AdminEventParticipationController {
 
     @Operation(
             summary = "행사 및 뒤풀이 수동 신청 (회원)",
-            description = "관리자가 회원의 정보를 바탕으로 행사를 수동으로 신청 처리합니다. 이벤트 뒤풀이가 있는 행사라면 뒤풀이 참석 상태로 신청처리합니다.")
+            description = "관리자가 회원의 정보를 바탕으로 행사를 수동으로 신청 처리합니다. 이벤트 뒤풀이가 있는 행사라면 뒤풀이 신청 상태로 신청 처리합니다.")
     @PostMapping("/apply/manual/registered")
     public ResponseEntity<Void> applyManualForRegistered(@Valid @RequestBody EventRegisteredApplyRequest request) {
         eventParticipationService.applyManualForRegistered(request);
@@ -96,7 +96,7 @@ public class AdminEventParticipationController {
 
     @Operation(
             summary = "행사 및 뒤풀이 수동 신청 (비회원)",
-            description = "관리자가 비회원의 정보를 바탕으로 행사를 수동으로 신청 처리합니다. 이벤트 뒤풀이가 있는 행사라면 뒤풀이 참석 상태로 신청처리합니다.")
+            description = "관리자가 비회원의 정보를 바탕으로 행사를 수동으로 신청 처리합니다. 이벤트 뒤풀이가 있는 행사라면 뒤풀이 신청 상태로 신청 처리합니다.")
     @PostMapping("/apply/manual/unregistered")
     public ResponseEntity<Void> applyManualForUnregistered(@Valid @RequestBody EventUnregisteredApplyRequest request) {
         eventParticipationService.applyManualForUnregistered(request);
