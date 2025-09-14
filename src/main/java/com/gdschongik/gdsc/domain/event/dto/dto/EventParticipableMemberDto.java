38b47efe70e92abfda89ec24884a85485a057976
chosw1002@naver.com <chosw1002@naver.com>
2025-09-14 22:07:27 +0900

    feat: 행사 참여 가능 멤버 검색 api에 전화번호 필드 추가

--- src/main/java/com/gdschongik/gdsc/domain/event/dto/dto/EventParticipableMemberDto.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/dto/dto/EventParticipableMemberDto.java
@@ -2,8 +2,10 @@ package com.gdschongik.gdsc.domain.event.dto.dto;
 
 import com.gdschongik.gdsc.domain.member.domain.Member;
 
-public record EventParticipableMemberDto(Long memberId, String name, String studentId, boolean participable) {
+public record EventParticipableMemberDto(
+        Long memberId, String name, String studentId, String phone, boolean participable) {
     public static EventParticipableMemberDto from(Member member, boolean participable) {
-        return new EventParticipableMemberDto(member.getId(), member.getName(), member.getStudentId(), participable);
+        return new EventParticipableMemberDto(
+                member.getId(), member.getName(), member.getStudentId(), member.getPhone(), participable);
     }
 }
