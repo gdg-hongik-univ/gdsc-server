1629485fcca144dcd244d821f535c205e4e2609c
Jaehyun Ahn <desktop3711@gmail.com>
2025-10-28 21:27:00 +0900

    feat: deprecated 대신 API 제거로 변경

--- src/main/java/com/gdschongik/gdsc/domain/event/api/ParticipantEventController.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/api/ParticipantEventController.java
@@ -1,29 +1,12 @@
 package com.gdschongik.gdsc.domain.event.api;
 
-import com.gdschongik.gdsc.domain.event.application.EventService;
-import com.gdschongik.gdsc.domain.event.dto.dto.EventDto;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Deprecated
 @Tag(name = "Event - Participant", description = "참가자용 행사 관리 API입니다.")
 @RestController
 @RequestMapping("/participant/events")
 @RequiredArgsConstructor
-public class ParticipantEventController {
-
-    private final EventService eventService;
-
-    @Operation(summary = "행사 조회", description = "행사를 조회합니다.")
-    @GetMapping("/{eventId}")
-    public ResponseEntity<EventDto> getEvent(@PathVariable Long eventId) {
-        var response = eventService.getEvent(eventId);
-        return ResponseEntity.ok(response);
-    }
-}
+public class ParticipantEventController {}
