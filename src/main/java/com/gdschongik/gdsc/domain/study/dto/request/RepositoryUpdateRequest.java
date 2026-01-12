88714b5f7321ff212d44e20a5ed826bd4f2f5a65
Jaehyun An <uwoobeat@gmail.com>
2026-01-12 20:24:49 +0900

    refactor: Study V1 Domain 레이어 제거

--- src/main/java/com/gdschongik/gdsc/domain/study/dto/request/RepositoryUpdateRequest.java
+++
@@ -1,8 +0,0 @@
-package com.gdschongik.gdsc.domain.study.dto.request;
-
-import com.gdschongik.gdsc.global.common.constant.RegexConstant;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-
-public record RepositoryUpdateRequest(
-        @NotBlank @Pattern(regexp = RegexConstant.GITHUB_REPOSITORY) String repositoryLink) {}
