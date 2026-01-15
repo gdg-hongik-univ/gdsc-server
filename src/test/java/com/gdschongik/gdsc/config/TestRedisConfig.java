423c6f79117b5be2b39de982e3028b6bcd9c8dc3
kimsh1017 <kimsh8580@gmail.com>
2026-01-15 22:56:51 +0900

    refactor: 포트 지정 로직 OS 의존하지 않도록 변경

--- src/test/java/com/gdschongik/gdsc/config/TestRedisConfig.java
+++ src/test/java/com/gdschongik/gdsc/config/TestRedisConfig.java
@@ -2,27 +2,25 @@ package com.gdschongik.gdsc.config;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.net.ServerSocket;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.util.StringUtils;
 import redis.embedded.RedisServer;
 
 @TestConfiguration
 public class TestRedisConfig {
 
-    private int port = 6379;
+    private int port;
 
     private RedisServer redisServer;
 
     @PostConstruct
     public void redisServer() throws IOException {
-        port = isRedisRunning() ? findAvailablePort() : port;
+        port = findAvailablePort();
         redisServer = new RedisServer(port);
         redisServer.start();
     }
@@ -41,52 +39,14 @@ public class TestRedisConfig {
     }
 
     /**
-     * Embedded Redis가 현재 실행중인지 확인
-     */
-    private boolean isRedisRunning() throws IOException {
-        return isRunning(executeGrepProcessCommand(port));
-    }
-
-    /**
      * 현재 PC/서버에서 사용가능한 포트 조회
      */
-    public int findAvailablePort() throws IOException {
-
-        for (int port = 10000; port <= 65535; port++) {
-            Process process = executeGrepProcessCommand(port);
-            if (!isRunning(process)) {
-                return port;
-            }
+    private int findAvailablePort() {
+        try (ServerSocket socket = new ServerSocket(0)) { // 사용 가능한 TCP 포트 할당
+            socket.setReuseAddress(false); // 포트 재사용 방지
+            return socket.getLocalPort();
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to allocate a TCP port", e);
         }
-
-        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
-    }
-
-    /**
-     * 해당 port를 사용중인 프로세스 확인하는 sh 실행
-     */
-    private Process executeGrepProcessCommand(int port) throws IOException {
-        String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
-        String[] shell = {"/bin/sh", "-c", command};
-        return Runtime.getRuntime().exec(shell);
-    }
-
-    /**
-     * 해당 Process가 현재 실행중인지 확인
-     */
-    private boolean isRunning(Process process) {
-        String line;
-        StringBuilder pidInfo = new StringBuilder();
-
-        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-
-            while ((line = input.readLine()) != null) {
-                pidInfo.append(line);
-            }
-
-        } catch (Exception e) {
-        }
-
-        return !StringUtils.isEmpty(pidInfo.toString());
     }
 }
