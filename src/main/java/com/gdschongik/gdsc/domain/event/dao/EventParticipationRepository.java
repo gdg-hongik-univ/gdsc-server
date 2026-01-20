73fe5e14114d9dd574e68750b522e56ffbcb7b84
chosw1002@naver.com <chosw1002@naver.com>
2026-01-20 20:55:28 +0900

    remove: 사용하지 않는 메서드 제거

--- src/main/java/com/gdschongik/gdsc/domain/event/dao/EventParticipationRepository.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/dao/EventParticipationRepository.java
@@ -13,8 +13,6 @@ public interface EventParticipationRepository
 
     List<EventParticipation> findAllByEventOrderByParticipantName(Event event);
 
-    long countByEvent(Event event);
-
     boolean existsByEvent(Event event);
 
     boolean existsByEventAndParticipantStudentId(Event event, String studentId);
