f65fd175747a8cbb4c3681f46b2af716401e2115
Jaewon Kim <eshcirc1e@gmail.com>
2026-02-09 21:35:31 +0900

    refactor: MemberDomainService에서 null 값 검증방식 변경

--- src/main/java/com/gdschongik/gdsc/domain/member/domain/service/MemberDomainService.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/domain/service/MemberDomainService.java
@@ -2,8 +2,6 @@ package com.gdschongik.gdsc.domain.member.domain.service;
 
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.global.annotation.DomainService;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 @DomainService
 public class MemberDomainService {
@@ -13,18 +11,18 @@ public class MemberDomainService {
      * 주어진 member의 디스코드 유저네임이 아니면서 이미 존재하는 유저네임이라면 충돌이라고 판단합니다.
      */
     public boolean determineDiscordUsernameDuplicateWithOthers(
-            @NotNull Member member, @NotBlank String discordUsername, boolean alreadyExists) {
-        boolean isMemberDiscordUsername = discordUsername.equals(member.getDiscordUsername());
+            Member member, String discordUsername, boolean alreadyExists) {
+        boolean isMemberDiscordUsername =
+                discordUsername != null && discordUsername.equals(member.getDiscordUsername());
         return !isMemberDiscordUsername && alreadyExists;
     }
 
     /**
      * 주어진 member를 제외한 다른 멤버와의 닉네임 충돌 여부를 판단합니다.
-     * 주어진 member의 닉네임이 아니면서 닉네임이라면 충돌이라고 판단합니다.
+     * 주어진 member의 닉네임이 아니면서 이미 존재하는 닉네임이라면 충돌이라고 판단합니다.
      */
-    public boolean determineNicknameDuplicateWithOthers(
-            @NotNull Member member, @NotBlank String nickname, boolean alreadyExists) {
-        boolean isMemberNickname = nickname.equals(member.getNickname());
+    public boolean determineNicknameDuplicateWithOthers(Member member, String nickname, boolean alreadyExists) {
+        boolean isMemberNickname = nickname != null && nickname.equals(member.getNickname());
         return !isMemberNickname && alreadyExists;
     }
 }
