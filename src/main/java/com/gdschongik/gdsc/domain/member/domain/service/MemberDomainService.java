6784baac2d0ec21b99a05d605f3b1d1f6ee3e4b7
Jaewon Kim <eshcirc1e@gmail.com>
2026-02-14 23:01:58 +0900

    refactor: 이미 존재하는 디스코드 유저명, 닉네임에 대한 불리언 변수를 명확한 네이밍을 사용하도록 변경

--- src/main/java/com/gdschongik/gdsc/domain/member/domain/service/MemberDomainService.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/domain/service/MemberDomainService.java
@@ -11,18 +11,18 @@ public class MemberDomainService {
      * 주어진 member의 디스코드 유저네임이 아니면서 이미 존재하는 유저네임이라면 충돌이라고 판단합니다.
      */
     public boolean determineDiscordUsernameDuplicate(
-            Member member, String discordUsernameToUse, boolean alreadyExists) {
+            Member member, String discordUsernameToUse, boolean discordUsernameAlreadyExists) {
         boolean isMyDiscordUsername =
                 discordUsernameToUse != null && discordUsernameToUse.equals(member.getDiscordUsername());
-        return !isMyDiscordUsername && alreadyExists;
+        return !isMyDiscordUsername && discordUsernameAlreadyExists;
     }
 
     /**
      * 주어진 member를 제외한 다른 멤버와의 닉네임 충돌 여부를 판단합니다.
      * 주어진 member의 닉네임이 아니면서 이미 존재하는 닉네임이라면 충돌이라고 판단합니다.
      */
-    public boolean determineNicknameDuplicate(Member member, String nicknameToUse, boolean alreadyExists) {
+    public boolean determineNicknameDuplicate(Member member, String nicknameToUse, boolean nicknameAlreadyExists) {
         boolean isMyNickname = nicknameToUse != null && nicknameToUse.equals(member.getNickname());
-        return !isMyNickname && alreadyExists;
+        return !isMyNickname && nicknameAlreadyExists;
     }
 }
