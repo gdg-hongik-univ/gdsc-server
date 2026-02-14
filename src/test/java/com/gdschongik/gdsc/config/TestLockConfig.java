15db058c9f1df22c7ad8a7e37a2f0def4fb449e5
kimsh1017 <kimsh8580@gmail.com>
2026-02-14 22:18:56 +0900

    test: TestLockConfig 임시 deprecated

--- src/test/java/com/gdschongik/gdsc/config/TestLockConfig.java
+++ src/test/java/com/gdschongik/gdsc/config/TestLockConfig.java
@@ -1,12 +1,15 @@
 package com.gdschongik.gdsc.config;
 
+import com.gdschongik.gdsc.global.lock.InmemoryLockUtil;
 import com.gdschongik.gdsc.global.lock.LockUtil;
-import com.gdschongik.gdsc.helper.InmemoryLockUtil;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
-@TestConfiguration
+/**
+ * 테스트 환경에서 인메모리 락을 사용하기 위한 설정 클래스입니다.
+ * PostgreSQL 락 유틸이 구현이 완료될 때까지 테스트용 설정을 임시 deprecated 처리하였습니다.
+ */
+@Deprecated
 public class TestLockConfig {
 
     @Primary
