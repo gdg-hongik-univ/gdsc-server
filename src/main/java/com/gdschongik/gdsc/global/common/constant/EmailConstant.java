c3cd3a71653fbaccfb8baddd0c3461531f91aaec
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2026-02-15 22:30:42 +0900

    refactor: Email 로직에 남아있는 GDSC String 검토 (#1320)
    rename: 학회 이름 변경사항을 메일 발송자 정보에 반영

--- src/main/java/com/gdschongik/gdsc/global/common/constant/EmailConstant.java
+++ src/main/java/com/gdschongik/gdsc/global/common/constant/EmailConstant.java
@@ -5,9 +5,9 @@ public class EmailConstant {
     public static final String VERIFY_EMAIL_API_ENDPOINT = "/onboarding/verify-email?%s=";
     public static final String VERIFY_EMAIL_REQUEST_PARAMETER_KEY = "token";
     public static final String HONGIK_UNIV_MAIL_DOMAIN = "@g.hongik.ac.kr";
-    public static final String SENDER_PERSONAL = "GDSC Hongik";
+    public static final String SENDER_PERSONAL = "GDGoC Hongik";
     public static final String SENDER_ADDRESS = "gdsc.hongik@gmail.com";
-    public static final String VERIFICATION_EMAIL_SUBJECT = "GDSC Hongik 이메일 인증 코드입니다.";
+    public static final String VERIFICATION_EMAIL_SUBJECT = "GDGoC Hongik 이메일 인증 코드입니다.";
     public static final String TOKEN_EMAIL_NAME = "email";
 
     private EmailConstant() {}
