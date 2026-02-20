36e7ef689db9a5347b534fa668d568039c89d502
kimsh1017 <81338287+kimsh1017@users.noreply.github.com>
2026-02-20 14:27:27 +0900

    refactor: spotless 적용

--- src/main/java/com/gdschongik/gdsc/domain/email/application/EmailVerificationService.java
+++ src/main/java/com/gdschongik/gdsc/domain/email/application/EmailVerificationService.java
@@ -7,7 +7,6 @@ import com.gdschongik.gdsc.domain.email.domain.EmailVerification;
 import com.gdschongik.gdsc.domain.email.domain.event.PreviousEmailVerifiedEvent;
 import com.gdschongik.gdsc.domain.email.dto.request.PreviousEmailVerificationRequest;
 import com.gdschongik.gdsc.global.exception.CustomException;
-import com.gdschongik.gdsc.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
@@ -27,9 +26,9 @@ public class EmailVerificationService {
     public Long verifyPreviousMemberEmail(PreviousEmailVerificationRequest request) {
         EmailVerification emailVerification = emailVerificationRepository
                 .findById(request.token())
-                .orElseThrow(() -> new CustomException(ErrorCode.EMAIL_VERIFICATION_NOT_FOUND));
-        applicationEventPublisher.publishEvent(
-                new PreviousEmailVerifiedEvent(emailVerification.getCurrentMemberId(), emailVerification.getPreviousMemberId()));
+                .orElseThrow(() -> new CustomException(EMAIL_VERIFICATION_NOT_FOUND));
+        applicationEventPublisher.publishEvent(new PreviousEmailVerifiedEvent(
+                emailVerification.getCurrentMemberId(), emailVerification.getPreviousMemberId()));
         log.info(
                 "[EmailVerificationService] 이메일 인증 완료: currentMemberId={}, previousMemberId={}",
                 emailVerification.getCurrentMemberId(),
