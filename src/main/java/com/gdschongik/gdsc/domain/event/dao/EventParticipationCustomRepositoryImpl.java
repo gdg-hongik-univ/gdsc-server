8a47069368f8475c9c20c91632c2bdb5fb082d78
chosw1002@naver.com <chosw1002@naver.com>
2026-02-26 16:46:10 +0900

    fix: 이벤트 신청 후 탈퇴한 회원 null 문제 해결

--- src/main/java/com/gdschongik/gdsc/domain/event/dao/EventParticipationCustomRepositoryImpl.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/dao/EventParticipationCustomRepositoryImpl.java
@@ -50,7 +50,7 @@ public class EventParticipationCustomRepositoryImpl
         List<EventApplicantResponse> fetch = jpaQueryFactory
                 .select(new QEventApplicantResponse(eventParticipation, member))
                 .from(eventParticipation)
-                .leftJoin(member)
+                .innerJoin(member)
                 .on(eqMemberId())
                 .where(eventParticipation.id.in(ids))
                 .orderBy(orderSpecifiers)
