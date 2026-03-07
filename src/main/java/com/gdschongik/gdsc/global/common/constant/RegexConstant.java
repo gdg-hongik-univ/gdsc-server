6837fb4fd70592ac3ea349eb2698193f55951799
Jaewon Kim <eshcirc1e@gmail.com>
2026-03-07 20:05:43 +0900

    fix: 학교 이메일 정규표현식 수정

--- src/main/java/com/gdschongik/gdsc/global/common/constant/RegexConstant.java
+++ src/main/java/com/gdschongik/gdsc/global/common/constant/RegexConstant.java
@@ -7,7 +7,7 @@ public class RegexConstant {
     public static final String PHONE_WITHOUT_HYPHEN = "^010[0-9]{8}$";
     public static final String NICKNAME = "[ㄱ-ㅣ가-힣]{1,6}$";
     public static final String DEPARTMENT = "^D[0-9]{3}$";
-    public static final String HONGIK_EMAIL = "^[^\\W&=+'-+,<>]+(\\.[^\\W&=+'-+,<>]+)*@g\\.hongik\\.ac\\.kr$";
+    public static final String HONGIK_EMAIL = "^[^\\W&=+'+,<>]+(\\.[^\\W&=+'+,<>]+)*@g\\.hongik\\.ac\\.kr$";
     public static final String DATETIME = "yyyy-MM-dd'T'HH:mm:ss";
     public static final String ZONED_DATETIME = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
     public static final String DATE = "yyyy-MM-dd";
