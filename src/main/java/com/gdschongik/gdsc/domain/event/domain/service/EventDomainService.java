efa649b4055ec8760a5f7b06e60824c74f373a1d
kckc0608 <kckc0608@naver.com>
2026-06-23 21:30:50 +0900

    chore: main event의 경우 '본행사' 라는 용어를 사용하도록 통일

--- src/main/java/com/gdschongik/gdsc/domain/event/domain/service/EventDomainService.java
+++ src/main/java/com/gdschongik/gdsc/domain/event/domain/service/EventDomainService.java
@@ -14,8 +14,8 @@ import java.time.LocalDateTime;
 public class EventDomainService {
 
     /**
-     * 이벤트를 기본 정보를 변경합니다.
-     * @param regularRoleOnlyStatus 정회원 전용 이벤트 여부. 이미 신청자가 존재하는 경우 변경할 수 없습니다.
+     * 행사 기본 정보를 변경합니다.
+     * @param regularRoleOnlyStatus 정회원 전용 행사 여부. 이미 신청자가 존재하는 경우 변경할 수 없습니다.
      * @param mainEventMaxApplicantCount 변경하려는 본 행사 최대 신청자 수. (인원 제한이 없는 경우 null)
      * @param afterPartyMaxApplicantCount 변경하려는 뒤풀이 최대 신청자 수. (인원 제한이 없는 경우 null)
      * @param currentMainEventApplicantCount 현재 본 행사 신청자 수. EventParticipationRepository 조회 데이터
@@ -53,7 +53,7 @@ public class EventDomainService {
     }
 
     /**
-     * 이벤트의 최대 신청자 수를 변경할 때, 현재 신청자 수보다 낮게 설정하는 경우 예외를 발생시킵니다.
+     * 행사 또는 뒤풀이의 최대 신청자 수를 변경할 때, 현재 신청자 수보다 낮게 설정하는 경우 예외를 발생시킵니다.
      */
     private void validateUpdateMaxApplicantCount(long currentApplicantCount, @Nullable Integer newMaxApplicantCount) {
         if (newMaxApplicantCount == null) { // 인원 제한을 없애는 경우 항상 허용
@@ -75,9 +75,9 @@ public class EventDomainService {
     }
 
     /**
-     * 이벤트 신청 폼 정보를 변경합니다.
+     * 행사 신청 폼 정보를 변경합니다.
      * 이미 신청자가 존재하는 경우 수정할 수 없습니다.
-     * @param eventParticipationExists 현재 본 행사 신청 정보가 존재하는지 유무. EventParticipationRepository 조회 데이터
+     * @param eventParticipationExists 현재 행사 신청 정보가 존재하는지 유무. EventParticipationRepository 조회 데이터
      */
     public void updateFormInfo(
             Event event,
