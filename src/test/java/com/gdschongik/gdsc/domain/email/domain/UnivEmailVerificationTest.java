09a837e9437e0daf5bab490bc093d856543ece9d
kimsh1017 <kimsh8580@gmail.com>
2026-08-11 22:26:49 +0900

    refactor: 엔티티 구조 변경

---
+++ src/test/java/com/gdschongik/gdsc/domain/email/domain/UnivEmailVerificationTest.java
@@ -0,0 +1,59 @@
+package com.gdschongik.gdsc.domain.email.domain;
+
+import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
+import static org.assertj.core.api.Assertions.*;
+
+import com.gdschongik.gdsc.global.exception.CustomException;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class UnivEmailVerificationTest {
+
+    private static final Long MEMBER_ID = 1L;
+    private static final String UNIV_EMAIL = "test@g.hongik.ac.kr";
+    private static final String CODE = "042917";
+    private static final long TTL = 60L;
+
+    @Nested
+    class 인증정보_생성시 {
+
+        @Test
+        void 학교_메일과_인증_코드가_내부에_저장된다() {
+            // when
+            UnivEmailVerification univEmailVerification =
+                    UnivEmailVerification.create(MEMBER_ID, UNIV_EMAIL, CODE, TTL);
+
+            // then
+            assertThat(univEmailVerification.getMemberId()).isEqualTo(MEMBER_ID);
+            assertThat(univEmailVerification.getUnivEmail()).isEqualTo(UNIV_EMAIL);
+            assertThat(univEmailVerification.getCode()).isEqualTo(CODE);
+            assertThat(univEmailVerification.getTtl()).isEqualTo(TTL);
+        }
+    }
+
+    @Nested
+    class 인증_코드_검증시 {
+
+        @Test
+        void 코드가_일치하면_성공한다() {
+            // given
+            UnivEmailVerification univEmailVerification =
+                    UnivEmailVerification.create(MEMBER_ID, UNIV_EMAIL, CODE, TTL);
+
+            // when & then
+            assertThatCode(() -> univEmailVerification.validateCode(CODE)).doesNotThrowAnyException();
+        }
+
+        @Test
+        void 코드가_일치하지_않으면_실패한다() {
+            // given
+            UnivEmailVerification univEmailVerification =
+                    UnivEmailVerification.create(MEMBER_ID, UNIV_EMAIL, CODE, TTL);
+
+            // when & then
+            assertThatThrownBy(() -> univEmailVerification.validateCode("999999"))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(EMAIL_VERIFICATION_CODE_MISMATCH.getMessage());
+        }
+    }
+}
