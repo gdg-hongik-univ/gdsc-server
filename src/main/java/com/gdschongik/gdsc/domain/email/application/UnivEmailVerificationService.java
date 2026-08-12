f228da93d7ffc276afd02c53af807ede0c9f80f9
kimsh1017 <kimsh8580@gmail.com>
2026-08-12 20:58:44 +0900

    refactor: 코드 인증 로직 변경

--- src/main/java/com/gdschongik/gdsc/domain/email/application/UnivEmailVerificationService.java
+++ src/main/java/com/gdschongik/gdsc/domain/email/application/UnivEmailVerificationService.java
@@ -1,54 +1,49 @@
 package com.gdschongik.gdsc.domain.email.application;
 
+import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
+
 import com.gdschongik.gdsc.domain.email.dao.UnivEmailVerificationRepository;
 import com.gdschongik.gdsc.domain.email.domain.UnivEmailVerification;
-import com.gdschongik.gdsc.domain.email.domain.service.UnivEmailValidator;
 import com.gdschongik.gdsc.domain.email.dto.request.UnivEmailVerificationRequest;
-import com.gdschongik.gdsc.domain.email.dto.request.UnivEmailVerificationTokenDto;
 import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.global.exception.CustomException;
-import com.gdschongik.gdsc.global.exception.ErrorCode;
-import com.gdschongik.gdsc.global.util.email.EmailVerificationTokenUtil;
+import com.gdschongik.gdsc.global.util.MemberUtil;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UnivEmailVerificationService {
 
-    private final EmailVerificationTokenUtil emailVerificationTokenUtil;
     private final MemberRepository memberRepository;
     private final UnivEmailVerificationRepository univEmailVerificationRepository;
-    private final UnivEmailValidator univEmailValidator;
+    private final MemberUtil memberUtil;
 
     @Transactional
     public void verifyMemberUnivEmail(UnivEmailVerificationRequest request) {
-        UnivEmailVerificationTokenDto emailVerificationToken = getEmailVerificationToken(request.token());
-        Member member = getMemberById(emailVerificationToken.memberId());
-        member.completeUnivEmailVerification(emailVerificationToken.email());
-        memberRepository.save(member);
+        Member currentMember = memberUtil.getCurrentMember();
+        UnivEmailVerification univEmailVerification = univEmailVerificationRepository
+                .findById(currentMember.getId())
+                .orElseThrow(() -> new CustomException(UNIV_EMAIL_VERIFICATION_CODE_NOT_SENT));
+        univEmailVerification.validateCode(request.code());
+
+        // TODO: 어플리케이션 이벤트 발행 방식으로 변경
+        currentMember.completeUnivEmailVerification(univEmailVerification.getUnivEmail());
+        memberRepository.save(currentMember);
+        univEmailVerificationRepository.delete(univEmailVerification);
+
+        log.info(
+                "[UnivEmailVerificationService] 학교 이메일 인증 완료: memberId={}, univEmail={}",
+                currentMember.getId(),
+                univEmailVerification.getUnivEmail());
     }
 
     public Optional<UnivEmailVerification> getUnivEmailVerificationFromRedis(Long memberId) {
         return univEmailVerificationRepository.findById(memberId);
-    }
-
-    private UnivEmailVerificationTokenDto getEmailVerificationToken(String verificationToken) {
-        UnivEmailVerificationTokenDto univEmailVerificationTokenDto =
-                emailVerificationTokenUtil.parseEmailVerificationTokenDto(verificationToken);
-        final Optional<UnivEmailVerification> univEmailVerification =
-                getUnivEmailVerificationFromRedis(univEmailVerificationTokenDto.memberId());
-
-        univEmailValidator.validateUnivEmailVerification(univEmailVerification, verificationToken);
-
-        return univEmailVerificationTokenDto;
-    }
-
-    private Member getMemberById(Long id) {
-        return memberRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
     }
 }
--- src/main/java/com/gdschongik/gdsc/domain/email/domain/service/UnivEmailValidator.java
+++ src/main/java/com/gdschongik/gdsc/domain/email/domain/service/UnivEmailValidator.java
@@ -4,10 +4,8 @@ import static com.gdschongik.gdsc.global.common.constant.EmailConstant.HONGIK_UN
 import static com.gdschongik.gdsc.global.common.constant.RegexConstant.HONGIK_EMAIL;
 import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
-import com.gdschongik.gdsc.domain.email.domain.UnivEmailVerification;
 import com.gdschongik.gdsc.global.annotation.DomainService;
 import com.gdschongik.gdsc.global.exception.CustomException;
-import java.util.Optional;
 
 @DomainService
 public class UnivEmailValidator {
@@ -29,22 +27,6 @@ public class UnivEmailValidator {
 
         if (isUnivEmailDuplicate) {
             throw new CustomException(UNIV_EMAIL_ALREADY_SATISFIED);
-        }
-    }
-
-    /**
-     * redis 안의 존재하는 메일인증 정보로 학교 메일을 검증
-     * 1. 토큰이 비었는데 인증하려할 시 에러 (인증메일을 보내지 않았거나, 만료된 경우)
-     * 2. 토큰이 redis에 저장된 토큰과 다르면 만료되었다는 에러 (메일 여러번 보낸 경우)
-     */
-    public void validateUnivEmailVerification(
-            Optional<UnivEmailVerification> optionalUnivEmailVerification, String currentToken) {
-        if (optionalUnivEmailVerification.isEmpty()) {
-            throw new CustomException(EMAIL_NOT_SENT);
-        }
-
-        if (!optionalUnivEmailVerification.get().getVerificationToken().equals(currentToken)) {
-            throw new CustomException(EXPIRED_EMAIL_VERIFICATION_TOKEN);
         }
     }
 }
